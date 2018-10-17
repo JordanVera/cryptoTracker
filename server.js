85a443ca7d6e19ab6ed4c2c43171086550a2eb31
Jordan Vera <verawebdevelopment@gmail.com>
2018-10-16 19:12:41 -0500

    added route config to server.js

--- server.js
+++ server.js
@@ -27,10 +27,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cryptoTracker",
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, '/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function (req, res) {
-      res.sendFile(path.join(__dirname, '/build', 'index.html'));
-    });
   };
 
 // const seedArr = [
